d505377d0471ffc168711618ad56be7558d848ad
detricb1 <detricbrown@go.rmc.edu>
2026-05-06 13:40:34 -0400

    Initial version of CourseInfo

---
+++ CourseInfo.java
@@ -0,0 +1,20 @@
+/* CourseInfo.java
+ *
+ * Name: Detric Brown
+ * Date: 05/6/2026
+ *
+ * Description: This program uses basic Java functions to print the hard-coded
+ * information of a course.
+ *
+ */
+
+
+public class CourseInfo {
+	public static void main(String[] args) {
+		System.out.println("Course Name: Onject-Oriented Programming");
+		System.out.println("Instructor: Dr. Elouni");
+		System.out.println("Semester: Spring 2026");
+		System.out.println("Credits: 4");
+		System.out.println("Classroom: Copley 204");
+	}
+}
